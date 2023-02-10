04cc43cf9e51417ed313c688f97842731fdc6d3f
Alex Abbey <alexabbey4@gmail.com>
2023-02-10 12:41:14 -0700

    starting to look at the dataframe

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import json
+import sys
+import pandas as pd
 import numpy as np
 
 layout = np.zeros(shape=(18, 11), dtype=int)
 
 if __name__ == "__main__":
     with open('problems.json') as p:
-        d = json.load(p)
+        d = json.load(p)  # dict of total, data
+
+    boulder_data = d['data']  # list of dicts, info for all problems in dataset
+    data_df = pd.DataFrame(boulder_data)  # dataframe for all the info about the boulders
+    print(data_df.keys())
+
+
+    sys.exit()
 
     print(d['data'][10])
     for k, v in d['data'][10].items():
